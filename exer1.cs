4e8e9679ab4abd8d0889b590d006f316bdfe531b
Vcolla <85914982+Vcolla@users.noreply.github.com>
2021-06-20 13:09:18 -0300

    Add files via upload

---
+++ exer1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace calculando_dias_dos_meses_ex_1
+{
+  
+  
+        class Program
+        {
+            static void Main(string[] args)
+            {
+
+                int mes;
+                
+               
+
+            
+                Console.WriteLine("Calculadora de dias em meses");
+              
+
+                Console.Write("Por favor, informe quanto(s) mes(es) deseja saber a quantidade de dia(s): ");
+                mes = Convert.ToInt32(Console.ReadLine());
+
+               
+                Console.WriteLine(mes * 30 );
+
+            }
+            
+            }
+        }
+
